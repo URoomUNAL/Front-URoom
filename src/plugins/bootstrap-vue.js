44b35c47c8fae5427da635aab0f0903121b1b66b
Ricardo Andrés Calvo <rcalvom@unal.edu.co>
2021-03-30 16:05:18 -0500

    Header terminado

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 
 import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+/*import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'*/
+import '../assets/styles/color.scss'
+import '../assets/styles/fonts.scss'
+import '../assets/styles/styles.scss'
 
 Vue.use(BootstrapVue)
