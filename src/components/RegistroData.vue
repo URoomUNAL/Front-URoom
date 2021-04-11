a486163ad1ef07a5c1be3f76c158f7f5e627dbc5
Diego Felipe <dlopezav@unal.edu.co>
2021-04-11 13:27:18 -0500

    avoiding overwritten

--- src/components/RegistroData.vue
+++ src/components/RegistroData.vue
@@ -67,7 +67,7 @@
             
             <b-card class="mt-3" header="Form Data Result">
               <pre class="m-0">{{ form }}</pre>
-            </b-card>-->
+            </b-card>
 
           </b-container>
         </b-form>
