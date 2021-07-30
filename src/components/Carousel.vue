f8cedc7eacf22f2fe762cfb06d1ebbb0a5e97947
Ricardo Andres Calvo Mendez <rcalvom@unal.edu.co>
2021-07-30 03:15:00 -0500

    Ajustes finales

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <b-carousel id="carousel-fade" style="text-shadow: 0px 0px 2px #000" fade indicators img-width="2" img-height="2">
-      <b-carousel-slide caption="Imágen 1" img-src="https://picsum.photos/1024/480/?image=10"/>
-      <b-carousel-slide caption="Imágen 2" img-src="https://picsum.photos/1024/480/?image=12"/>
-      <b-carousel-slide caption="Imágen 3" img-src="https://picsum.photos/1024/480/?image=22"/>
+      <b-carousel-slide caption="Imágen 1" img-src="https://en.estelarparquedela93.com/uploads/galeriahabitaciones/estelar-parque-la-93.jpg"/>
+      <b-carousel-slide caption="Imágen 2" img-src="https://tecnohotelnews.com/wp-content/uploads/2018/01/accorhotels-smart-room.jpg"/>
+      <b-carousel-slide caption="Imágen 3" img-src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/242279547.jpg?k=4e9fa5ac15c00663e0d464fccd4bd73ba67bdbc81993da4d3a4871112666f713&o=&hp=1"/>
     </b-carousel>
   </div>
 </template>
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,14 +1,25 @@
 <template>
-  <b-container class="mb-5">
+  <b-container class="mb-5 text-left">
     <b-row>
       <b-col>
-        <h1 class="primary">Sobre Nosotros</h1>
+        <h1 class="primary mb-3 text-center">Sobre Nosotros</h1>
       </b-col>
     </b-row>
     <b-row>
-      <p>Somos estudiantes de la Universidad Nacional de Colombia</p>
+      <p>
+        Somos estudiantes de la Universidad Nacional de Colombia y hemos realizado este proyecto para la asignatura de Ingeniería de Software II
+        No somo hemos diseñado URoom como un proyecto de software, nos interesa que este pueda ayudar a los estudiantes a encontrar la habitación
+        que mas se ajusta a sus necesidades.
+      </p>
+      <p>
+        Trabajamos continuamente para traer una plataforma mas amigable y con mucha mas visibilidad, puedes apoyarnos compartiendo esta página
+        a personas que le pueda interesar, ya sean estudiantes o arrendadores, esto nos ayudará a crecer y entregar un mejor servicio.
+      </p>
+      <p>
+        Los miembros del equipo de desarrollo de URoom son:
+      </p>
     </b-row>
-    <b-row>
+    <b-row align-h="center">
       <b-col>
         <ul>
           <li><p>Ricardo Andrés Calvo Méndez</p></li>
@@ -19,12 +30,13 @@
         </ul>
       </b-col>
     </b-row>
-    <b-row align-h="center" align-v="center">
+    <b-row align-v="center" align-h="center" class="text-center mr-0 ml-0">
       <b-col align-self="center">
-        <b-icon-github font-scale="3.0"/>        
-      </b-col>
-      <b-col align-self="center">
-        URoomUnal
+        <p>
+          <a href="https://github.com/URoomUNAL" class="secondary align-middle"><b-icon-github font-scale="3.0"/></a>
+           &nbsp;
+          <a href="https://github.com/URoomUNAL" class="secondary align-align-middle-top">Github URoom Unal</a>
+        </p>
       </b-col>
     </b-row>
   </b-container>
